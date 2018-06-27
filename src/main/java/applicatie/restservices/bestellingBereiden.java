ef0262cead6fda1f8be90ace32236989db516b0b
Eduard Garritsen <31863537+EduardGarritsen@users.noreply.github.com>
2018-06-27 07:54:05 +0200

    Add files via upload

--- src/main/java/applicatie/restservices/bestellingBereiden.java
+++ src/main/java/applicatie/restservices/bestellingBereiden.java
@@ -4,20 +4,54 @@ import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 
-import applicatie.dao.bestellingDao;
-import applicatie.dao.bestellingDaoImpl;
-import applicatie.model.bestelling;
+import applicatie.dao.*;
+import applicatie.model.*;
 
 @Path("/bestelling")
 public class bestellingBereiden {
 	@GET
-	@Path("/bereiden")
+	@Path("/afleveren")
 	public String getBestellingen() {
+		
+		bestellingDao bDoa = new bestellingDaoImpl();
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+		
+		String status = "gereed";
+		
+		for (bestelling b : bDoa.selectBestellingByStatus(status)) {
+			JsonObjectBuilder job = Json.createObjectBuilder();
+			job.add("bestellingId", b.getBestelling_ID());
+			job.add("Tafel", b.getTafel());
+			job.add("gerecht", b.getGerecht());
+			job.add("prijs", b.getPrijs());
+			job.add("klantId", b.getKlant_Id());
+			jab.add(job);
+		}
+		
+		
+		JsonArray bestellingenInAfwachting = jab.build();
+		
+		System.out.println(bestellingenInAfwachting);
+		
+		return bestellingenInAfwachting.toString();
+	}
+	
+	@POST
+	@Path("/afgeleverd")
+	public void deleteBestelling(@FormParam("bestellingId") int bestelling_Id) {
+		
+		System.out.println(bestelling_Id);
+		
+		bestellingDao bDoa = new bestellingDaoImpl();
+		
+		bDoa.deleteBestellingById(bestelling_Id);
+	}
+	
+	@GET
+	@Path("/bereiden")
+	public String getBestellingen2() {
 		
 		bestellingDao bDoa = new bestellingDaoImpl();
 		JsonArrayBuilder jab = Json.createArrayBuilder();
@@ -44,7 +78,7 @@ public class bestellingBereiden {
 	
 	@POST
 	@Path("/bereden")
-	public void deleteBestelling(@FormParam("bestellingId") int bestelling_Id) {
+	public void updateBestelling(@FormParam("bestellingId") int bestelling_Id) {
 		
 		System.out.println(bestelling_Id);
 		String status = "gereed";
@@ -54,4 +88,3 @@ public class bestellingBereiden {
 		bDoa.updateBestelling(bestelling_Id, status);
 	}
 }
-
