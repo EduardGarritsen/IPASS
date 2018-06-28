e42142f2d943f602ff6064479baea407e166bb15
Eduard Garritsen <31863537+EduardGarritsen@users.noreply.github.com>
2018-06-28 08:24:32 +0200

    Add files via upload

--- src/main/java/applicatie/dao/bestellingDao.java
+++ src/main/java/applicatie/dao/bestellingDao.java
@@ -10,5 +10,4 @@ public interface bestellingDao {
 	public bestelling insertBestelling(bestelling bestelling);
 	public bestelling updateBestelling(int bestelling_ID, String status);
 	public bestelling deleteBestellingById(int bestelling_ID);
-	public void getRows();
 }
